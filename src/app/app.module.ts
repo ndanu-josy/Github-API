b15432405b303ed9c2b2de93135e50ceade93c45
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-21 15:55:25 +0300

    Generate classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
