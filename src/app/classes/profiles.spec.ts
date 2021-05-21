bf02b6cc0b1e11335cd180c3ad0680c7a12d7a2e
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-21 16:16:18 +0300

    classes

---
+++ src/app/classes/profiles.spec.ts
@@ -0,0 +1,7 @@
+import { Profiles } from './profiles';
+
+describe('Profiles', () => {
+  it('should create an instance', () => {
+    expect(new Profiles()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/repos.ts
@@ -0,0 +1,9 @@
+export class Repos {
+
+    constructor(public name: string,
+        public description: string,
+        public url: string,
+        public language: string,
+        public updated_at: Date) {
+}
+}
