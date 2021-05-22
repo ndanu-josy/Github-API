66307d512323a9b854292032178c3fde22c70f81
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-22 11:34:08 +0300

    store apikey in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key: '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',
+  api_url:'https://api.github.com/users/'
 };
 
 /*
