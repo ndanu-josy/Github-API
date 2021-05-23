22d0590fee369ad59ae5ad5c2aa060d23b044d69
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-24 00:46:38 +0300

    highlight

--- src/app/mycustom-directive.directive.ts
+++ src/app/mycustom-directive.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appMycustomDirective]'
 })
 export class MycustomDirectiveDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('teal');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
