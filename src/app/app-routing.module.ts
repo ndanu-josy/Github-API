b2d50b4b09af80fdcf2cddfe257e46067349964a
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-24 00:33:22 +0300

    not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: UserprofileComponent },
+  { path: 'about', component: AboutComponent},
+  { path:'**', component:NotFoundComponent},
+  { path: '', redirectTo:"/home", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { UserprofileComponent } from './userprofile/userprofile.component';
 import { DateCountPipe } from './date-count.pipe';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,    
-    UserprofileComponent, DateCountPipe, AboutComponent, NavbarComponent,
+    UserprofileComponent, DateCountPipe, AboutComponent, NavbarComponent, NotFoundComponent,
     
   ],
   imports: [
